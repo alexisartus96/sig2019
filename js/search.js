0380027289f5681efce4ac6a3e53047c79eeabda
joaquin.villar <joacovillar93@gmail.com>
2019-11-12 19:39:35 -0300

    Search filter by USA

--- js/search.js
+++ js/search.js
@@ -1,14 +1,23 @@
 require([
+    "esri/tasks/Locator",
     "esri/widgets/Search"
     ], 
-    function(Search) {
+    function(Locator, Search) {
       searchPopup.content = "<div class='searchPopup'><h4>Añadir punto</h4><button type='button' value='Agregar parada' class='w3-button w3-xlarge w3-circle w3-red w3-card-4' onclick='addStop(mapPoint); view.popup.close()'>+</button></div>"
 
       var search = new Search({
+        sources: [{
+          locator: new Locator({ url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"}),
+          countryCode:"US",
+          placeholder: "Search USA",
+          suggestionsEnabled: true,
+          minSuggestCharacters: 0
+      }],
         container: "search",
         popupEnabled: true,
         view: view,
-        resultGraphicEnabled: true
+        resultGraphicEnabled: true,
+        includeDefaultSources: false
       }, "search");
 
       search.popupTemplate = searchPopup;
